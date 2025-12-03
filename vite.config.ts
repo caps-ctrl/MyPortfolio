e68b734e9e475883f3db5359bf12aae51134b725
Marcel Gusiew <gusiew9@gmail.com>
2025-12-03 15:54:08 +0100

    add opti

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,18 @@ import { imagetools } from "vite-imagetools";
 export default defineConfig({
   plugins: [react(), tailwindcss(), imagetools()],
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  build: {
+    minify: "esbuild",
+    target: "es2020",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+
+          galaxy: ["./src/components/Galaxy.tsx"],
+        },
+      },
+    },
+  },
 });
